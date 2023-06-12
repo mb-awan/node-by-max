ae3225f46d6245fe964fd8cfb48ef0293ef1cdec
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-06-12 11:23:19 +0500

    redirecting the request and parsing the request body and fetching the string data out of it and saved it in the file in append mode

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // Understanding the request
-  console.log({
-    reqUrl: req.url,
-    reqMethod: req.method,
-    reqHeader: req.headers,
-  });
+  const url = req.url;
+  const method = req.method;
+  const body = [];
+  if (url === "/") {
+    res.write(`
+	<html>
+		<head>
+			<title>Save Message</title>
+		</head>
+		<body>
+			<h1>Enter The Message</h1>
+			<form action = '/message' method = 'POST'>
+				<input type = 'text' name = 'message'>
+				<button type = 'submit' >Save</button>
+			</form>
+		</body>
+	</html>
+	`);
+	return res.end();
+  }
 
-  // Sending the response
+  if(url === '/message' && method === 'POST') {
+	req.on('data', chunk => {
+		body.push(chunk);
+	})
 
-  res.setHeader('content-type','text/html');
-  res.write('<html>');
-  res.write('<head><title>My first response</title></head>');
-  res.write('<body><h1>Hello from Node.js</h1></body>')
-  res.write('</html>');
-  console.log({res});
-  res.end();
-  console.log({res});
-//   process.exit(); // Playing with event Loop
+	req.on('end', () => {
+		const parsedBody = Buffer.concat(body).toString();
+		console.log(parsedBody);
+		const message = parsedBody.split('=')[1];
+		fs.appendFileSync('message.txt', message);
+		res.statusCode = 303;
+		res.setHeader('location', '/');
+		return res.end();
+	})
+  }
 });
 
 const PORT = 3000;
