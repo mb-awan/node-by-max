8d9e2d802bf37c5d83cc4115ec2d56fdee730ee3
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-06-13 09:53:34 +0500

    adding middlewares - express is all about middlewares

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const http = require('http');
+const http = require("http");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+// we can use middleware here.
+
+app.use((req, res, next) => {
+  // app.use is used to use middlewares
+  // This middle ware will be executed on each request
+  // nex is used to redirect the request to the next middleware in a sequence
+  console.log("In the first middleware");
+  next(); // will call the next middleware on this line and when the control comeback then execute the next line
+});
+
+app.use((req, res, next) => {
+  console.log("In the second middleware");
+});
+
 const server = http.createServer(app);
 
 const PORT = 3000;
