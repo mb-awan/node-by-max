7d4602cf8f3d5cb68034b859e020868af713f4ab
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-06-13 11:14:06 +0500

    limiting and differenciating middleware execution to POST request

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true })); // To parse request body before each middleware
 
-app.use("/add-product", (req, res) => {
+app.use("/add-products", (req, res) => {
   res.send(`
     <form action = "/products" method = "POST">
         <input type="text" name = "prod-name"/>
@@ -13,12 +13,12 @@ app.use("/add-product", (req, res) => {
     `);
 });
 
-app.use("/products", (req, res) => {
+app.post("/products", (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
 
-app.use("/all-products", (req, res, next) => {
+app.get("/products", (req, res, next) => {
   res.send("<h1>This is 'ALL Products' page</h1>");
 });
 
