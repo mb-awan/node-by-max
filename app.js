302c766a1cb10fad904ad25e11db7e7f58a60470
Muhammad Bilal <bilalofficialmail123@gmail.com>
2023-06-13 11:01:58 +0500

    parse body from incomming routes - used both express.urlEncoded and body-parser.urlEncoded external request body parser libarary

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const express = require("express");
 
 const app = express();
 
-// app.use has many overloads out which we can use app.use([PATH-optional],callback) to manage different routes
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded( { extended: true})); is deprecated and built in now in express
+// will parse each request body to js object before each request
+
+app.use(express.urlencoded({ extended: true })); // Latest
+
+app.use("/add-product", (req, res) => {
+  res.send(`
+    <form action = "/products" method = "POST">
+        <input type="text" name = "prod-name"/>
+        <button type = "submit">Add</button>
+    </form>
+    `);
+});
+
+app.use("/products", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
 app.use("/all-products", (req, res, next) => {
   res.send("<h1>This is 'ALL Products' page</h1>");
 });
 
 app.use("/", (req, res, next) => {
-  res.send("<h>Hello from express js</h1>");
+  res.send("<h1>Hello from express js</h1>");
 });
 
 const PORT = 3000;
